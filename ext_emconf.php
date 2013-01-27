7ee11b80587c0121e7a1c6f701de4cbee552b298
tuurlijk <tuurlijk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-27 21:15:34 +0000

    Added two new templates
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sponsoring404/trunk@70326 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,21 @@
 <?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "sponsoring404".
+#
+# Auto generated 27-01-2013 21:13
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => '404 Sponsoring',
 	'description' => 'Sponsor charity with your 404 page',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.1.0',
+	'version' => '1.3.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -28,11 +39,11 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-		)
+		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:9:"Changelog";s:4:"b8b4";s:10:"README.txt";s:4:"2972";s:30:"class.tx_ncstaticfilecache.php";s:4:"864c";s:42:"class.tx_ncstaticfilecache_crawlerhook.php";s:4:"e98e";s:16:"ext_autoload.php";s:4:"eb7f";s:21:"ext_conf_template.txt";s:4:"177b";s:12:"ext_icon.gif";s:4:"03df";s:17:"ext_localconf.php";s:4:"c470";s:14:"ext_tables.php";s:4:"378e";s:14:"ext_tables.sql";s:4:"209c";s:16:"locallang_db.xml";s:4:"a157";s:15:"cli/cleaner.php";s:4:"df75";s:14:"doc/_.htaccess";s:4:"99c8";s:25:"doc/gzip.realurl.htaccess";s:4:"1fe7";s:32:"doc/gzip.simulateStatic.htaccess";s:4:"dd32";s:14:"doc/manual.sxw";s:4:"53a1";s:26:"doc/plain.realurl.htaccess";s:4:"9617";s:33:"doc/plain.simulateStatic.htaccess";s:4:"7a3d";s:52:"infomodule/class.tx_ncstaticfilecache_infomodule.php";s:4:"0b9b";s:24:"infomodule/locallang.php";s:4:"022c";s:60:"tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages.php";s:4:"d4f0";s:84:"tasks/class.tx_ncstaticfilecache_tasks_processDirtyPages_AdditionalFieldProvider.php";s:4:"7779";s:61:"tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages.php";s:4:"3e57";s:85:"tasks/class.tx_ncstaticfilecache_tasks_removeExpiredPages_AdditionalFieldProvider.php";s:4:"7109";}',
+	'_md5_values_when_last_written' => 'a:51:{s:24:"atlassian-ide-plugin.xml";s:4:"41db";s:12:"ext_icon.gif";s:4:"71c8";s:14:"ext_tables.php";s:4:"6a03";s:36:"Classes/Controller/404Controller.php";s:4:"7fae";s:38:"Configuration/TypoScript/constants.txt";s:4:"fa65";s:34:"Configuration/TypoScript/setup.txt";s:4:"cd54";s:40:"Resources/Private/Language/locallang.xml";s:4:"4a0f";s:43:"Resources/Private/Language/nl.locallang.xml";s:4:"019d";s:57:"Resources/Private/Templates/bartfoundation/background.jpg";s:4:"757f";s:53:"Resources/Private/Templates/bartfoundation/index.html";s:4:"1ce9";s:66:"Resources/Private/Templates/bartfoundation2/backgroundcompleet.png";s:4:"fab4";s:56:"Resources/Private/Templates/bartfoundation2/bartlogo.png";s:4:"70fe";s:54:"Resources/Private/Templates/bartfoundation2/index.html";s:4:"5618";s:45:"Resources/Private/Templates/books4life/bg.png";s:4:"cd3d";s:49:"Resources/Private/Templates/books4life/index.html";s:4:"e6c7";s:48:"Resources/Private/Templates/books4life/intro.png";s:4:"824d";s:54:"Resources/Private/Templates/books4life/label-green.png";s:4:"89db";s:52:"Resources/Private/Templates/books4life/label-red.png";s:4:"1f5b";s:50:"Resources/Private/Templates/books4life/right-1.png";s:4:"9497";s:50:"Resources/Private/Templates/books4life/right-2.png";s:4:"b767";s:46:"Resources/Private/Templates/books4life/tag.png";s:4:"5006";s:48:"Resources/Private/Templates/books4life/title.png";s:4:"1bcb";s:57:"Resources/Private/Templates/ikkominactie1/achtergrond.png";s:4:"07ce";s:56:"Resources/Private/Templates/ikkominactie1/home_sweet.png";s:4:"9e88";s:52:"Resources/Private/Templates/ikkominactie1/index.html";s:4:"3c35";s:49:"Resources/Private/Templates/ikkominactie1/kop.png";s:4:"4117";s:63:"Resources/Private/Templates/ikkominactie1/logo_ikkominactie.png";s:4:"6ad4";s:53:"Resources/Private/Templates/ikkominactie1/ontwerp.jpg";s:4:"6c14";s:53:"Resources/Private/Templates/ikkominactie1/ontwerp.png";s:4:"a559";s:64:"Resources/Private/Templates/ikkominactie1/tekening_geraniums.png";s:4:"3c64";s:48:"Resources/Private/Templates/ikkominactie2/bg.jpg";s:4:"0c23";s:58:"Resources/Private/Templates/ikkominactie2/ikkominactie.png";s:4:"4d36";s:52:"Resources/Private/Templates/ikkominactie2/index.html";s:4:"74a4";s:63:"Resources/Private/Templates/imcweekendschool1/buitengebruik.png";s:4:"6b66";s:56:"Resources/Private/Templates/imcweekendschool1/index.html";s:4:"2c9a";s:62:"Resources/Private/Templates/imcweekendschool1/logo_furture.png";s:4:"67f3";s:58:"Resources/Private/Templates/imcweekendschool1/logo_imc.png";s:4:"9b3f";s:51:"Resources/Private/Templates/nierstichting/gozer.png";s:4:"fade";s:52:"Resources/Private/Templates/nierstichting/index.html";s:4:"0fc7";s:62:"Resources/Private/Templates/nierstichting/middenbackground.jpg";s:4:"1d78";s:54:"Resources/Private/Templates/nierstichting/nierlogo.jpg";s:4:"d0aa";s:59:"Resources/Private/Templates/nierstichting/topbackground.jpg";s:4:"749d";s:54:"Resources/Private/Templates/villaticca1/background.png";s:4:"07ba";s:50:"Resources/Private/Templates/villaticca1/index.html";s:4:"4423";s:47:"Resources/Private/Templates/villaticca1/koe.png";s:4:"9d1c";s:48:"Resources/Private/Templates/villaticca1/logo.png";s:4:"8e2f";s:49:"Resources/Private/Templates/villaticca1/titel.png";s:4:"ef65";s:46:"Resources/Private/Templates/villaticca2/bg.png";s:4:"d31f";s:50:"Resources/Private/Templates/villaticca2/index.html";s:4:"02ed";s:48:"Resources/Private/Templates/villaticca2/logo.png";s:4:"74f8";s:14:"doc/manual.sxw";s:4:"d573";}',
 	'suggests' => array(
-	)
+	),
 );
 
 ?>
